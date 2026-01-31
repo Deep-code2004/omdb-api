6d4dc5ac2b80d40419d7d4d078c2eca8a8d94e67
Deep Khatri <deepkhatri050@gmail.com>
2026-02-01 00:03:36 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base:"/OMDB-API---Example",
+      base:"/omdb-api",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
